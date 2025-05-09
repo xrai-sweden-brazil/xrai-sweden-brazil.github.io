6c07866a94a8de012525c19b5cb81e4f41a5edd3
dcm0 <donald.mcmillan@dsv.su.se>
2025-05-09 16:57:24 +0200

    building

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/xrai-sweden-brazil.github.io/" : "/", // Update this to your deployment path
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
